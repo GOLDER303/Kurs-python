67a1b19570b34cf455be0016dc2bcb15e6643314
GOLDER303 <szymonstepien@interia.pl>
2021-09-21 19:37:41 +0200

    Pobieranie wartosci z sys.argv

--- z01.py
+++ z01.py
@@ -1,8 +1,13 @@
+import sys
 import math
 
-a = float(input("Podaj a:"))
-b = float(input("Podaj b:"))
-c = float(input("Podaj c:"))
+# a = float(input("Podaj a:"))
+# b = float(input("Podaj b:"))
+# c = float(input("Podaj c:"))
+
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+c = float(sys.argv[3])
 
 delta = b**2 - 4*a*c 
 
@@ -10,7 +15,6 @@ if delta > 0:
     x1 = (-b + math.sqrt(delta))/2*a
     x2 = (-b - math.sqrt(delta))/2*a
     print(f"X1: {x1}\nX2: {x2}")
-    print('X1: ' + x1 + '\nX2: ' + x2)
 
 elif delta == 0:
     x0 = -b/2*a
