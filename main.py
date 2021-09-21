bd136f7296ee51505e33748a6b88db203f3fb7d9
GOLDER303 <szymonstepien@interia.pl>
2021-09-21 18:49:38 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+import sys
+import math
+
+'''
+print("Hello World")
+
+number = 2
+print(type(number))
+number = 2.1
+print(type(number))
+
+name = 'Szymon'
+print(type(name))
+
+t = False
+print(type(t))
+
+n = None
+print(type(n))
+
+massage = f"Wiek {name} to {number}"
+massage2 = "Wiek {} to {}".format(name, number)
+massage3 = "Wiek %s to %s" % (name, number)
+
+n = 2.0
+print(type(n))
+nn = float(n)
+print(type(nn))
+
+name = input("Podaj imie:")
+age = int(input("Podaj wiek:"))
+print(name, age)
+print(type(name), type(age))
+
+#print(sys.argv) # argumenty podane za nazwą programu przy wywołaniu
+
+lista = ["Adam", 23, True, None, 124, 23, 421, 21, 44]
+
+# print(lista[2])
+# print(lista[-1])
+# print(lista[0:2])
+# print(lista[:]) # wsyzstko 
+# print(lista[0:7:2]) # co drugi
+# name = "Adam"
+# print(name[::-1]) # odwracanie stringa
+
+lista.append("Nowak")
+lista.insert(1,3)
+lista.remove(23)
+temp = lista.pop(0)
+print(temp)
+del lista[0]
+
+print(lista)
+print(len(lista))
+'''
