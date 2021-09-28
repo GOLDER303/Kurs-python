5d64eba70addd9a4ba16019fecf93640a71f145f
GOLDER303 <szymonstepien@interia.pl>
2021-09-28 16:28:39 +0200

    Optymalizated for fewer lines of code

--- pesel_control_number/main.py
+++ pesel_control_number/main.py
@@ -1,16 +1,10 @@
 pesel = input("Podaj pesel: ")
 
+numbers_for_multipicatoin = [1, 3, 7, 9, 1, 3, 7, 9, 1, 3]
 result = 0
-result += (int(pesel[0]) * 1) % 10
-result += (int(pesel[1]) * 3) % 10
-result += (int(pesel[2]) * 7) % 10
-result += (int(pesel[3]) * 9) % 10
-result += (int(pesel[4]) * 1) % 10
-result += (int(pesel[5]) * 3) % 10
-result += (int(pesel[6]) * 7) % 10
-result += (int(pesel[7]) * 9) % 10
-result += (int(pesel[8]) * 1) % 10
-result += (int(pesel[9]) * 3) % 10
+
+for i in range(len(pesel)):
+    result += (int(pesel[i]) * numbers_for_multipicatoin[i]) % 10
 
 result %= 10
 control_number = 10 - result
