376313b95eefcea19e2ad8a6910994445f5414f8
GOLDER303 <szymonstepien@interia.pl>
2021-10-07 15:47:29 +0200

    Added checking if given date is corect

--- weather/objectiv_weather.py
+++ weather/objectiv_weather.py
@@ -1,5 +1,6 @@
 import requests
 import sys
+from datetime import date as dt
 
 class Weather:
 
@@ -7,49 +8,56 @@ class Weather:
         self.api_key = api_key
         self.date = date
         self.localization = localization
+        self.years, self.months, self.days = self.date.split("-")
+        self.years, self.months, self.days = int(self.years), int(self.months), int(self.days)
+        self.current_date = dt.today()
+        if(self.years != self.current_date.year or self.months != self.current_date.month or self.days > self.current_date.day + 2 or self.days < self.current_date.day):
+            print("Podałeś złą datę! Prognoza jest możliwa jedynie na 2 dni do przodu")
+            exit()
+        self.day_index = int(self.days) - self.current_date.day
         self.data = self.get_data()
 
     def get_data_handler(self):
         pass
 
     def get_data(self):
-        URL = f"http://api.weatherapi.com/v1/forecast.json?key={self.api_key}&q={self.localization}&days=3"
+        URL = f"http://api.weatherapi.com/v1/forecast.json?key={self.api_key}&q={self.localization}&days={self.day_index + 1}"
         r = requests.get(URL)
         return r.json()
 
-    def get_avg_temp(self):        
-        avg_temp = self.data["forecast"]["forecastday"][1]["day"]["avgtemp_c"]
+    def get_avg_temp(self):    
+        avg_temp = self.data["forecast"]["forecastday"][self.day_index]["day"]["avgtemp_c"]
         return f"Średnioa temperatura {avg_temp} \N{DEGREE SIGN}C"
 
     def get_min_temp(self):
-        min_temp = self.data["forecast"]["forecastday"][1]["day"]["mintemp_c"]
+        min_temp = self.data["forecast"]["forecastday"][self.day_index]["day"]["mintemp_c"]
         return f"Minimalna temperatura {min_temp} \N{DEGREE SIGN}C"
 
     def get_max_temp(self):
-        max_temp = self.data["forecast"]["forecastday"][1]["day"]["maxtemp_c"]
+        max_temp = self.data["forecast"]["forecastday"][self.day_index]["day"]["maxtemp_c"]
         return f"Maksymalna temperatura {max_temp} \N{DEGREE SIGN}C"
 
     def get_avg_humidity(self):
-        avg_humidity = self.data["forecast"]["forecastday"][1]["day"]["avghumidity"]
+        avg_humidity = self.data["forecast"]["forecastday"][self.day_index]["day"]["avghumidity"]
         return f"Średnioa wilgotność {avg_humidity}%"
 
     def get_max_wind(self):
-        max_wind = self.data["forecast"]["forecastday"][1]["day"]["maxwind_kph"]
+        max_wind = self.data["forecast"]["forecastday"][self.day_index]["day"]["maxwind_kph"]
         return f"Maksymalna prędkość wiatru {max_wind} km/h"
 
     def get_rain(self):
-        rain = self.data["forecast"]["forecastday"][1]["day"]["totalprecip_mm"]
+        rain = self.data["forecast"]["forecastday"][self.day_index]["day"]["totalprecip_mm"]
         return "Będzie padać" if rain > 0 else "Nie będzie padało"
 
 api_key = sys.argv[1]
 date = sys.argv[2]
 localization = 'Zielona-Góra'
 
-w1 = Weather(api_key, date, localization)
+weather = Weather(api_key, date, localization)
 
-print(w1.get_avg_temp())
-print(w1.get_min_temp())
-print(w1.get_max_temp())
-print(w1.get_avg_humidity())
-print(w1.get_max_wind())
-print(w1.get_rain())
+print(weather.get_avg_temp())
+print(weather.get_min_temp())
+print(weather.get_max_temp())
+print(weather.get_avg_humidity())
+print(weather.get_max_wind())
+print(weather.get_rain())
